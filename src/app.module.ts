7b28574dc95ac871a18f858ad722987cfa58de94
CasasolaFranco <franco.casasola@estudiantes.unahur.edu.ar>
2024-11-12 16:44:45 -0300

    Conexion a mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://admin:DesApp@clusterdesapp.1esjv.mongodb.net/BD-GestionConvocatorias?retryWrites=true&w=majority&appName=ClusterDesApp')],
   controllers: [AppController],
   providers: [AppService],
 })
