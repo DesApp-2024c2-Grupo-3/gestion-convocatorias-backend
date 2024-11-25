b26673db3889a63ef438b05affcfba11d91e919c
PalNahu <nahuelmartin.palma@estudiantes.unahur.edu.ar>
2024-11-25 19:30:11 -0300

    Endpoint DELETE agregado

--- src/convocatorias/convocatorias.controller.ts
+++ src/convocatorias/convocatorias.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get, Param, Put } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Put, Delete } from '@nestjs/common';
 import { ConvocatoriasService } from './convocatorias.service';
 import { Convocatorias } from './convocatorias.schema';
 
@@ -23,9 +23,13 @@ export class ConvocatoriasController {
     return this.convocatoriasService.create(convocatoria);
   }
 
-  @Put(':id')
-  async updateConvocatoria(@Param('id') id:string, @Body() convocatoria:Convocatorias): Promise<any>{
-    return this.convocatoriasService.updateConvocatoria(id,convocatoria)
+  @Put(':_id')
+  async updateConvocatoria(@Param('_id') _id:string, @Body() convocatoria:Convocatorias): Promise<any>{
+    return this.convocatoriasService.updateConvocatoria(_id,convocatoria)
+  }
+  @Delete(':_id')
+  async eliminarConvocatoria(@Param('_id') _id: string){
+    return this.convocatoriasService.eliminarConvocatoria(_id);
   }
 
 
--- src/convocatorias/convocatorias.service.ts
+++ src/convocatorias/convocatorias.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, ConflictException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Convocatorias } from './convocatorias.schema';
 import { Model } from 'mongoose';
@@ -14,38 +14,55 @@ export class ConvocatoriasService {
     return this.convoctariasModel.find().exec();
   }
 
-  async getConvocatoria(id:string): Promise<Convocatorias> {
-    const convocatoriaExistente = await this.convoctariasModel.findById(id).exec()
+  async getConvocatoria(id: string): Promise<Convocatorias> {
+    const convocatoriaExistente = await this.convoctariasModel
+      .findById(id)
+      .exec();
 
-    if(!convocatoriaExistente){
-      
-      throw new BadRequestException("La convocatoria no existe")
+    if (!convocatoriaExistente) {
+      throw new BadRequestException('La convocatoria no existe');
     }
 
-    return convocatoriaExistente
+    return convocatoriaExistente;
   }
 
-
-  
-
-  async create(convocatoria: Convocatorias) {    
+  async create(convocatoria: Convocatorias) {
     const createdConvocatoria = new this.convoctariasModel(convocatoria);
-    return createdConvocatoria.save(); 
+    return createdConvocatoria.save();
   }
 
+  async updateConvocatoria(
+    _id: string,
+    convocatoria: Convocatorias,
+  ): Promise<Convocatorias> {
+    const convocatoriaActualizada = await this.convoctariasModel
+      .findByIdAndUpdate(
+        _id,
+        {
+          $set: convocatoria,
+        },
+        { new: true },
+      )
+      .exec();
 
-  
-  async updateConvocatoria(id: string, convocatoria: Convocatorias): Promise<Convocatorias> {
-    const convocatoriaActualizada = await this.convoctariasModel.findByIdAndUpdate(id, {
-      $set: convocatoria 
-    }, { new: true }).exec();
-  
     if (!convocatoriaActualizada) {
-      throw new BadRequestException("La convocatoria que desea actualizar, no existe");
+      throw new BadRequestException(
+        'La convocatoria que desea actualizar, no existe',
+      );
     }
-  
+
     return convocatoriaActualizada;
   }
 
+  async eliminarConvocatoria(_id: string) {
+    const convocatoriaExistente = await this.convoctariasModel
+      .findById(_id)
+      .exec();
 
+    if (!convocatoriaExistente) {
+      throw new BadRequestException('NO EXISTE');
+    }
+
+    await this.convoctariasModel.findByIdAndDelete(_id).exec();
+  }
 }
