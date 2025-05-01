ca5b55f0d744fda6b2806797a27fb8fcf48e05ca
CasasolaFranco <franco.casasola@estudiantes.unahur.edu.ar>
2025-05-01 20:25:41 -0300

    Implementados autenticacion con JWT y control de acceso por roles en modulo formato

--- src/formato/formato.controller.ts
+++ src/formato/formato.controller.ts
@@ -1,23 +1,31 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { FormatoService } from './formato.service';
 import { Formato } from './formato.schema';
 import { CreateFormatoDto } from './dtos/CreateFormatoDTO';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { HasRoles } from 'src/auth/decorators/has-roles.decorator';
+import { ROLES } from 'src/constants/roles';
 
 @Controller('formato')
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class FormatoController {
     constructor(private formatoService: FormatoService) {}
 
     @Get()
+    @HasRoles(ROLES.SUPER_ADMIN, ROLES.ADMIN)
     async get(): Promise<Formato[]> {
         return this.formatoService.getAllFormatos();
     }
 
     @Get(':id')
+    @HasRoles(ROLES.SUPER_ADMIN, ROLES.ADMIN, ROLES.INVESTIGADOR)
     async getFormatoById(@Param('id') id: string): Promise<Formato> {
         return this.formatoService.getFormatoById(id);
     }
 
     @Post()
+    @HasRoles(ROLES.SUPER_ADMIN, ROLES.ADMIN)
     async create (@Body() formato: CreateFormatoDto) {
         return this.formatoService.createFormato(formato)
     }
--- src/formato/formato.module.ts
+++ src/formato/formato.module.ts
@@ -3,6 +3,8 @@ import { FormatoController } from './formato.controller';
 import { FormatoService } from './formato.service';
 import { Formato, FormatoSchema } from './formato.schema';
 import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [FormatoController],
@@ -13,6 +15,13 @@ import { MongooseModule } from '@nestjs/mongoose';
             schema: FormatoSchema,
         },
     ]),
+    JwtModule.registerAsync({
+        inject: [ConfigService],
+        useFactory: (ConfigService: ConfigService) => ({
+            secret: ConfigService.get<string>("JWT_SECRET"),
+            signOptions: { expiresIn: '60m'}
+        })
+    })
   ],
   providers: [FormatoService],
   exports: [FormatoService]
