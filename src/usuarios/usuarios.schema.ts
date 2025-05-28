d8d30707ef0ec520582f4f40e15ed06d59fd5671
mateoburellab <mateoburellab03@gmail.com>
2025-05-28 19:59:59 -0300

    Agregada funcionalidad adaptada a Swagger para poder cargar CV de usuario

--- src/usuarios/usuarios.schema.ts
+++ src/usuarios/usuarios.schema.ts
@@ -5,9 +5,26 @@ import { ApiProperty, ApiSchema } from '@nestjs/swagger';
 export type UsuarioDocument = HydratedDocument<Usuario>;
 
 @Schema()
+export class Cv {
+  @ApiProperty({ example: 'curriculum.pdf' })
+  @Prop()
+  nombre: string;
+
+  @ApiProperty({ example: 'curriculum/pdf '})
+  @Prop()
+  tipo: string;
+
+  @ApiProperty({ description: 'Contenido del archivo en Buffer' })
+  @Prop()
+  contenido: Buffer
+}
+
+const CvSchema = SchemaFactory.createForClass(Cv)
+
+@Schema()
 @ApiSchema({ description: 'Schema que define la estructura de los documentos de la coleccion "Usuarios"' })
 export class Usuario {
-  @Prop({ required: true }) 
+  @Prop({ required: true })
   @ApiProperty({
     description: 'Nombre del usuario',
     example: 'Juan Perez',
@@ -16,16 +33,16 @@ export class Usuario {
   })
   nombre: string;
 
-  @Prop({ required: true, unique: true }) 
+  @Prop({ required: true, unique: true })
   @ApiProperty({
     description: 'Email del usuario',
     example: 'ejemplo@gmail.com',
     type: String,
     required: true
-})
+  })
   email: string;
 
-  @Prop({ required: true }) 
+  @Prop({ required: true })
   @ApiProperty({
     description: 'Contraseña del usuario',
     example: 'password123',
@@ -42,9 +59,9 @@ export class Usuario {
     default: [ROLES.INVESTIGADOR],
     enum: [ROLES.SUPER_ADMIN, ROLES.ADMIN, ROLES.INVESTIGADOR],
     enumName: 'ROLES'
-})
+  })
   roles: string[];
-  
+
   @Prop()
   @ApiProperty({
     description: 'Estado del usuario',
@@ -53,6 +70,16 @@ export class Usuario {
     default: false,
   })
   baja: Boolean;
+
+  @Prop({ type: CvSchema })
+  @ApiProperty({
+    description: 'Currículum del usuario',
+    type: Cv,
+    required: false,
+    default: false
+  })
+  //cv: { nombre: string; tipo: string; contenido: Buffer };
+  cv: Cv
 }
 
 export const UsuarioSchema = SchemaFactory.createForClass(Usuario);
