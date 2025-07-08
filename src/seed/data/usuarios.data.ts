d2ee566fa00edc296b612e8e6c6c94e0a7f04945
nicolas.delafuente <ni.delafuente@gmail.com>
2025-07-08 14:09:03 -0300

    chore: simplifico la contraseña de los usuarios seeders

--- src/seed/data/usuarios.data.ts
+++ src/seed/data/usuarios.data.ts
@@ -7,19 +7,19 @@ export const usuariosData = [
   {
     nombre: 'Super Administrador',
     email: configService.get<string>('SUPER_ADMIN_EMAIL') || 'superadmin@email.edu.ar',
-    password: configService.get<string>('SUPER_ADMIN_PASSWORD') || 'SuperAdmin123!',
+    password: configService.get<string>('SUPER_ADMIN_PASSWORD') || 'SuperAdmin123',
     roles: [ROLES.SUPER_ADMIN]
   },
   {
     nombre: 'Administrador General',
     email: 'admin@email.edu.ar',
-    password: 'Admin123!',
+    password: 'Admin123',
     roles: [ROLES.ADMIN]
   },
   {
     nombre: 'Investigador Principal',
     email: 'investigador@email.edu.ar',
-    password: 'Investigador123!',
+    password: 'Investigador123',
     roles: [ROLES.INVESTIGADOR]
   }
 ];
